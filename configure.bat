32a8eea78d2dc90041de6204c32a26797335fb1b
Benjamin-Elias Probst <benjamineliasprobst@gmail.com>
2025-12-27 15:30:08 +0100

    Replace legacy configure with wrapper-only entry

--- configure.bat
+++ configure.bat
@@ -1,21 +1,17 @@
 @echo off
-REM VIA Configuration Script for Windows
-REM Copyright (c) 2025 BEP Venture UG
+setlocal
 
-set BUILD_TYPE=Release
+set "SCRIPT_DIR=%~dp0"
+if "%BEP_BUILDSYSTEM_PATH%"=="" (
+  set "BUILDSYSTEM_DIR=%SCRIPT_DIR%third_party\BuildSystem"
+) else (
+  set "BUILDSYSTEM_DIR=%BEP_BUILDSYSTEM_PATH%"
+)
+set "INTERFACE=%BUILDSYSTEM_DIR%\scripts\interface.bat"
 
-:parse_args
-if "%~1"=="" goto end_parse
-if "%~1"=="--build-type=Release" set BUILD_TYPE=Release
-if "%~1"=="--build-type=Debug" set BUILD_TYPE=Debug
-if "%~1"=="--build-type=RelWithDebInfo" set BUILD_TYPE=RelWithDebInfo
-if "%~1"=="--build-type=MinSizeRel" set BUILD_TYPE=MinSizeRel
-shift
-goto parse_args
+if not exist "%INTERFACE%" (
+  echo [BEP][ERROR] BuildSystem interface not found: %INTERFACE%
+  exit /b 1
+)
 
-:end_parse
-echo Configuring VIA with build type: %BUILD_TYPE%
-if not exist cmake-build-%BUILD_TYPE% mkdir cmake-build-%BUILD_TYPE%
-cd cmake-build-%BUILD_TYPE%
-cmake -DCMAKE_BUILD_TYPE=%BUILD_TYPE% ..
-cd ..
+call "%INTERFACE%" "%SCRIPT_DIR%buildsystem.xml" "%SCRIPT_DIR%"
